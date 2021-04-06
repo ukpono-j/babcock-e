25103102a07345bb1e3dac19e8cd6c645789f32a
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-06 22:59:30 +0100

    setting up routing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
-// const { Router } = require("express");
 const express = require("express");
+const usersRoute = require("./users");
 
 const router = express.Router();
 
+router.use("/auth", usersRoute);
 
-router.get("/", (req, res) => res.send("welcome"));
+router.post("/files/:department", (req, res) => {
+  // find the department
+  // upload file
+  // and link to books Model and run association
+});
+
+router.get("/files/:fileName", (req, res) => {
+  // find the fileName
+  // pipe file stream to response
+});
 module.exports = router;
