0d7747b2ad953475518e9aac0b4e7e1f1ae4a7fa
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-07 21:21:54 +0100

    refactor route, add authentication to book route

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const usersRoute = require("./users");
+const booksRoute = require("./books");
+const isLoggedIn = require("../middlewares/is-logged-in");
 
 const router = express.Router();
 
 router.use("/auth", usersRoute);
+router.use("/books", isLoggedIn, booksRoute);
 
-router.post("/files/:department", (req, res) => {
-  // find the department
-  // upload file
-  // and link to books Model and run association
-});
-
-router.get("/files/:fileName", (req, res) => {
-  // find the fileName
-  // pipe file stream to response
-});
 module.exports = router;
