19c15c9a7e39c607fa4f5c00c9d94890043a3479
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-08 15:26:20 +0100

    redirect user to department page after sign-up

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,11 @@ const { user: User } = require("../models/index");
 module.exports = {
   _name: "auth",
   login: [
-    passport.authenticate("local"),
+    passport.authenticate("local", {
+      failureRedirect: "/login.html",
+    }),
     (req, res, next) => {
-      return res.json(req.user.toPublic());
+      return res.redirect("/department.html");
     },
   ],
 
@@ -19,12 +21,20 @@ module.exports = {
     });
   },
 
-  register: async (req, res) => {
-    try {
-      const user = await User.create(req.body);
-      res.created().json(user.toPublic());
-    } catch (err) {
-      res.internalServerError().send(err);
-    }
-  },
+  register: [
+    async (req, res) => {
+      try {
+        const user = await User.create(req.body);
+        res.created().json(user.toPublic());
+      } catch (err) {
+        res.internalServerError().send(err);
+      }
+    },
+    passport.authenticate("local", {
+      failureRedirect: "/login.html",
+    }),
+    (req, res, next) => {
+      return res.redirect("/department.html");
+    },
+  ],
 };
