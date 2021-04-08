e139b3dba5849bd962157afa0e0fa23db9a9e995
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-08 16:43:57 +0100

    redirect to login page

--- middlewares/is-logged-in.js
+++ middlewares/is-logged-in.js
@@ -1,4 +1,4 @@
 module.exports = function (req, res, next) {
-  if (!req.isAuthenticated()) return res.forbidden().end();
+  if (!req.isAuthenticated()) return res.forbidden().redirect("/login.html");
   return next();
 };
