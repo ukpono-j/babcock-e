fb71a5a6a290c819e2e006a9d7fedba3ee6be13f
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-08 15:27:35 +0100

    bug fix

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,7 @@ const books = require("../controllers/books");
 
 router
   .post("/:department/upload", books.create)
+  .get("/:department", books.getOne)
   .get("/:bookName", books.getOne);
 
 module.exports = router;
