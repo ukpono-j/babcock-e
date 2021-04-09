4388c1ad94ab151eef0781d2d52c37ca80d53ab7
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-09 12:37:17 +0100

    set redirect to appropriate route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const passport = require("../handlers/auth");
-const { user: User } = require("../models/index");
+const { User } = require("../models/index");
 
 module.exports = {
   _name: "auth",
@@ -22,18 +22,19 @@ module.exports = {
   },
 
   register: [
-    async (req, res) => {
+    async (req, res, next) => {
       try {
         const user = await User.create(req.body);
-        res.created().json(user.toPublic());
+        next();
       } catch (err) {
-        res.internalServerError().send(err);
+//         res.internalServerError().send(err);
+        res.redirect("/sign-up.html");
       }
     },
     passport.authenticate("local", {
       failureRedirect: "/login.html",
     }),
-    (req, res, next) => {
+    (req, res) => {
       return res.redirect("/department");
     },
   ],
