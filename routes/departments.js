f21234286b22d90cca7a51ed3495b8f314a785ae
micaiah-effiong <micaiah.effiong@gmail.com>
2021-04-09 12:43:03 +0100

    code refactor to use import book model

--- routes/departments.js
+++ routes/departments.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const path = require("path")
 const booksRoute = require("./books");
-const Books = require("../models/books");
+const {Book} = require("../models/index");
 
 router.use("/books", booksRoute);
 
 router.get("/", async(req, res)=>{
-  const book = await Book.find();
+  const book = await Book.findAll();
   res.render("department")
 })
 
 router.get("/:department/", function(req, res) {
+//   const book = await Book.findAll({where: {department:}});
   res.render("department")
 })
 
