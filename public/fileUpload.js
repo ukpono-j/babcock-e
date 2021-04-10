89d765e5fb52e56d9f4088fc3525f96e44b2737b
ukpono-j <ukponoakpan270@gmail.com>
2021-04-10 15:40:12 +0100

    This is the new change

---
+++ public/fileUpload.js
@@ -0,0 +1,12 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+FilePond.setOptions({
+   stylePanelAspectRatio: 150/100,
+   imageResizeTargetWidth: 50,
+   imageResizeTargetHeight:  50
+})
+
+FilePond.parse(document.body);
